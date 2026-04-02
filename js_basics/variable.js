6906371e5addbc6db836d7225802a0562590f5cd
rishabhtripath <131430278+rishabhtripath@users.noreply.github.com>
2026-04-02 12:07:22 +0000

    add some changes

--- js_basics/variable.js
+++ js_basics/variable.js
@@ -1,6 +1,10 @@
 const accountId = 12345
 let accountEmail = "rishabh@google.com"
 var accountPassword = "700725"
+let accountState
 
-console.log(accountEmail);
+console.log(accountPassword);
+console.table([accountPassword, accountEmail, accountId accountState])
 
+
+// Var is not used mostly
